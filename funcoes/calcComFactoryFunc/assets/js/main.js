02d8fd0ace588bf42077551c9e22a93b41e9e290
Caio <matossousacaio.cms@gmail.com>
2026-03-06 12:07:35 -0300

    refatora a função da calculadora para usar a sintaxe de construtor

--- funcoes/calcComFactoryFunc/assets/js/main.js
+++ funcoes/calcComFactoryFunc/assets/js/main.js
@@ -1,73 +1,72 @@
-function criaCalculadora() {
-    return {
-        display: document.querySelector('.display'),
-        
-        clearDisplay() {
-            this.display.value = '';
-        },
+function Calculadora() {
+    this.display = document.querySelector('.display');
 
-        btnValueToDisplay(value) {
-            this.display.value += value;
-        },
-        
-        delButton() {
-            this.display.value = this.display.value.slice(0, -1);
-        },
+    this.clearDisplay = () => {
+        this.display.value = '';
+    }
 
-        calculate() {
-            let conta = this.display.value;
+    this.btnValueToDisplay = (value) => {
+        this.display.value += value;
+    }
 
-            try {
-                conta = eval(conta);
+    this.delButton = () => {
+        this.display.value = this.display.value.slice(0, -1);
+    }
 
-                if(!conta) {
-                    alert('Conta inválida');
-                    return;
-                }
+    this.calculate = () => {
+        let conta = this.display.value;
 
-                this.display.value = String(conta);
-            }catch(e) {
+        try {
+            conta = eval(conta);
+
+            if(!conta) {
                 alert('Conta inválida');
                 return;
             }
-        },
-        
-        getClickButtons(){
-            document.addEventListener('click', (e) => {
-                const el = e.target;
 
-                if(el.classList.contains('btn-num')) {
-                    this.btnValueToDisplay(el.innerText);
-                }
-                // verificar se o botão clicado é o de limpar o display
-                if(el.classList.contains('btn-clear')) {
-                    this.clearDisplay();
-                }
-                // verificar se o botão clicado é o de calcular a expressão
-                if(el.classList.contains('btn-eq')) {
-                    this.calculate();
-                }
-                // verificar se o botão clicado é o de apagar um número
-                if(el.classList.contains('btn-del')) {
-                    this.delButton();
-                }
-            });
-        },
-
-        pressEnter() {
-            this.display.addEventListener('keyup', (e) => {
-                if(e.key === 'Enter') {
-                    this.calculate();
-                }
-            });
-        },
-        
-        inicia(){
-            this.getClickButtons();
-            this.pressEnter();
-        },
+            this.display.value = String(conta);
+        }catch(e) {
+            alert('Conta inválida');
+            return;
+        }
     }
+
+    this.getClickButtons = () => {
+        document.addEventListener('click', (e) => {
+            const el = e.target;
+
+            if(el.classList.contains('btn-num')) {
+                this.btnValueToDisplay(el.innerText);
+            }
+            // verificar se o botão clicado é o de limpar o display
+            if(el.classList.contains('btn-clear')) {
+                this.clearDisplay();
+            }
+            // verificar se o botão clicado é o de calcular a expressão
+            if(el.classList.contains('btn-eq')) {
+                this.calculate();
+            }
+            // verificar se o botão clicado é o de apagar um número
+            if(el.classList.contains('btn-del')) {
+                this.delButton();
+            }
+        });
+    }
+
+    this.pressEnter = () => {
+        this.display.addEventListener('keyup', (e) => {
+            if(e.keyCode === 13) {
+                this.calculate();
+            }
+        });
+    }
+    
+    this.inicia = () => {
+        this.getClickButtons();
+        this.pressEnter();
+    }
+    
 }
 
-const calculadora = criaCalculadora();
+const calculadora = new Calculadora();
 calculadora.inicia();
