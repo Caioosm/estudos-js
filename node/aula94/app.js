e04cdc1c7baef27d9f13dffffe927317d78a3559
Caio <matossousacaio.cms@gmail.com>
2026-04-13 18:32:16 -0300

    Add package.json for aula95 with express dependency

---
+++ node/aula94/app.js
@@ -0,0 +1,3 @@
+import { Calculo } from './mod.js';
+const calculo =new Calculo(5, 10);
+console.log(calculo.multiplicar());
---
+++ node/aula94/mod.js
@@ -0,0 +1,10 @@
+export class Calculo {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    multiplicar() {
+        return this.a * this.b;
+    }
+}
