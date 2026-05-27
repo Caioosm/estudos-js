4559fd3862a9e0a57f4e9b92b770776a02c82cab
Caio <matossousacaio.cms@gmail.com>
2026-05-27 10:51:44 -0300

    feat: adiciona página de login com estilos e configurações globais

---
+++ react/react-base/src/pages/login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+
+import { Title } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>Page</small>
+      </Title>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </Container>
+  );
+}
