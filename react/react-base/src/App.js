b4c3c9c047669c27282ea6ee3fc925994d6cefec
Caio <matossousacaio.cms@gmail.com>
2026-05-28 10:40:23 -0300

    feat: adiciona cabeçalho com ícones de navegação e configurações de estilo

--- react/react-base/src/App.js
+++ react/react-base/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Login from './pages/login';
 import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/header';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
