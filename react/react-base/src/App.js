3aa1a45ed2afa1b9bb0f219cba006432874c0635
Caio <matossousacaio.cms@gmail.com>
2026-05-29 10:32:35 -0300

    feat: adiciona roteamento com página 404 e atualiza dependências do React Router

--- react/react-base/src/App.js
+++ react/react-base/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
-import Login from './pages/login';
+import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/header';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </Router>
   );
 }
 
