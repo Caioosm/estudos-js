efea9db74edee1ec94ac9d554d0698abe832ecca
Caio <matossousacaio.cms@gmail.com>
2026-06-15 10:35:49 -0300

    feat: adiciona funcionalidade de exclusão de alunos com confirmação e feedback de erro

--- react/consumir-api/src/pages/alunos/index.js
+++ react/consumir-api/src/pages/alunos/index.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import { get } from 'lodash';
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa'
+import { FaUserCircle, FaEdit, FaWindowClose, FaExclamation } from 'react-icons/fa'
 
 import { Container } from '../../styles/GlobalStyles';
 import axios from '../../services/axios';
 import { AlunoContainer, ProfilePicture } from './styled';
 import Loading from '../../components/loading';
+import { toast } from 'react-toastify';
 
 export default function Alunos() {
   const [alunos, setAlunos] = useState([]);
@@ -23,6 +24,35 @@ export default function Alunos() {
     getAlunos();
   }, []);
 
+  const handleDeleteAsk = e => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute('display', 'block');
+    e.currentTarget.remove();
+  }
+
+  const handleConfirmDelete = async(e, id, index) => {
+    e.persist();
+    try {
+      setIsLoading(true);
+      await axios.delete(`/alunos/${id}`)
+      const novosAlunos = [...alunos];
+      novosAlunos.splice(index, 1);
+      setAlunos(novosAlunos);
+      setIsLoading(false);
+
+    } catch(err) {
+      const status = get(err, 'response.status', 0);
+      if(status == 401) {
+        toast.error("Voce precisa fazer login!");
+      }else {
+        toast.error("Ocorreu um erro ao tentar excluir o aluno!");
+      }
+
+      setIsLoading(false);
+    }
+  }
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
@@ -30,7 +60,7 @@ export default function Alunos() {
       <h1>Alunos</h1>
 
       <AlunoContainer>
-        {alunos.map(aluno => (
+        {alunos.map((aluno, index) => (
           <div key={String(aluno.id)}>
 
             <ProfilePicture>
@@ -48,9 +78,16 @@ export default function Alunos() {
               <FaEdit size={16}/>
             </Link>
 
-            <Link to={`/aluno/${aluno.id}/delete`} >
+            <Link onClick={handleDeleteAsk} to={`/aluno/${aluno.id}/delete`} >
               <FaWindowClose />
             </Link>
+
+            <FaExclamation
+              onClick={e => handleConfirmDelete(e, aluno.id, index)}
+              size={16}
+              display="none"
+              cursor="pointer"
+            />
           </div>
         ))}
       </AlunoContainer>
