cf251e5bcbc152c99c26e990d157fa5c25af2414
Caio <matossousacaio.cms@gmail.com>
2026-06-26 11:16:31 -0300

    feat: adiciona função semRetorno e objeto pessoa com método exibirNome

---
+++ cursoTS/src/A003-type-void/A003-type-any.ts
@@ -0,0 +1,16 @@
+function semRetorno(...args: string[]): void {
+  console.log(args.join(' '));
+}
+
+const pessoa = {
+  nome: 'nao',
+  sobrenome: 'sei',
+
+  exibirNome(): void {
+    console.log(this.nome + ' ' + this.sobrenome);
+  }
+};
+
+pessoa.exibirNome();
+
+semRetorno('nao', 'sei');
